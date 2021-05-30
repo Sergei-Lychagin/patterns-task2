0e7c29b2271a7edf0579ab4df48e6b0e64d9a49d
Sergei Lychagin <lychagin0@gmail.com>
2021-05-30 08:18:30 +0400

    правка AuthTest

--- src/test/java/ru/netology/web/AuthTest.java
+++ src/test/java/ru/netology/web/AuthTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import ru.netology.data.DataGenerator;
 import ru.netology.data.RegistrationDto;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -23,8 +25,8 @@ public class AuthTest {
         $("[name='login']").setValue(validActiveUser.getLogin());
         $("[name='password']").setValue(validActiveUser.getPassword());
         $("[data-test-id='action-login']").click();
-        $(".heading").waitUntil(exist, 5000);
-        $(".heading").shouldHave(matchesText("Личный кабинет"));
+        $(".heading").shouldBe(exist, Duration.ofDays(5000));
+        $(".heading").shouldHave(text("Личный кабинет"));
     }
 
     @Test
@@ -33,7 +35,7 @@ public class AuthTest {
         $("[name='login']").setValue(validBlockedUser.getLogin());
         $("[name='password']").setValue(validBlockedUser.getPassword());
         $("[data-test-id='action-login']").click();
-        $("[data-test-id='error-notification']").waitUntil(Condition.visible, 15000);
+        $("[data-test-id='error-notification']").shouldBe(Condition.visible, Duration.ofDays(15000));
     }
 
     @Test
@@ -42,7 +44,7 @@ public class AuthTest {
         $("[name='login']").setValue(inCorrectLogin.getLogin());
         $("[name='password']").setValue(inCorrectLogin.getPassword());
         $("[data-test-id='action-login']").click();
-        $("[data-test-id='error-notification']").waitUntil(Condition.visible, 15000);
+        $("[data-test-id='error-notification']").shouldBe(Condition.visible, Duration.ofDays(15000));
     }
 
     @Test
@@ -51,7 +53,7 @@ public class AuthTest {
         $("[name='login']").setValue(inCorrectPassword.getLogin());
         $("[name='password']").setValue(inCorrectPassword.getPassword());
         $("[data-test-id='action-login']").click();
-        $(".notification_status_error").waitUntil(visible, 5000);
-        $("[data-test-id=error-notification]").shouldHave(Condition.matchesText("Ошибка! Неверно указан логин или пароль"));
+        $(".notification_status_error").shouldBe(visible, Duration.ofDays(5000));
+        $("[data-test-id=error-notification]").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"));
     }
 }
